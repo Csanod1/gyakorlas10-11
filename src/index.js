811d6fbd1bd403a53a0b971ab78fea3e4f7b6eb9
Kormos Csanad <csanadsuli19@gmail.com>
2022-10-11 12:03:05 +0200

    feladat2 kész

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let t = [];
-    
+    let tValogatott = [];
+    for(let i = 0; i< 20; i++){
+        t.push(Math.round(Math.random()*30+1));
+    }
+    tValogatott = t.filter(e => (e%5)==0);
+    console.log(tValogatott);
+    document.getElementById('oszthatoGomb').addEventListener('click', () =>{
+        
+        tValogatott = t.filter(e => (e%5)==0);
+        console.log(tValogatott);
+    });
 });
 
